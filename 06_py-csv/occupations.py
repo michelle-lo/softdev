bd61ffc0bbec7d7048116790288dfdbe29df1dcc
michelle.lo <mlo20@stuy.edu>
2021-09-29 01:24:43 -0400

    edited code for readibility + added new alternative method

--- 06_py-csv/occupations.py
+++ 06_py-csv/occupations.py
@@ -12,24 +12,44 @@ import csv
 import random
 
 #reading csv file
-with open('occupations.csv') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter= ",")
-    jobs = {} #dictionary created
-    i = 0;
-    for row in spamreader:
-        if (i == 0):
-            i += 1 #skips heading
-        else:
-            jobs[row[0]] = float(row[1]);
-        
+def reader():
+    with open('occupations.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter= ",")
+        jobs = {} #dictionary created
+        i = 0;
+        for row in spamreader:
+            if (i == 0):
+                i += 1 #skips heading
+            else:
+                jobs[row[0]] = float(row[1]);
+        return jobs
+            
 #random selector
-r = random.random() #generates a random float from 0 to 1
-s = 0
-for key in jobs: #for every occupation
-    s+= jobs[key]/100 #add occupation's percentage to s
-    if(s > r): #if combined percentages is larger than random number,
-        print(key) #return final occupation
-        break
+def randSelect(jobs):
+    r = random.random() #generates a random float from 0 to 1
+    s = 0
+    for key in jobs: #for every occupation
+        s+= jobs[key]/100 #add occupation's percentage to s
+        if(s > r): #if combined percentages is larger than random number,
+            #print(key) #return final occupation
+            #break
+            return key
+
+#randChoices utilizes random.choices to randomly choose an element based on its weighted probability
+def randChoices(jobs):
+    jobList = list(jobs.keys()) #turns dict keys into a list
+    percentages = list(jobs.values()) #dict values into a list
+    fin = random.choices(jobList, weights = percentages, k = 1) #generates a list of 1 randomly chosen occupation
+    return fin[0]
+
+def main():#runs program
+    print(randSelect(reader())) 
+    print(randChoices(reader()))
+
+if __name__ == '__main__':
+    main()
+    
+
 
     
 
