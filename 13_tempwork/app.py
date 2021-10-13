f4491acaa5844522b3c9b9ac8ca0a4f113dec4be
michelle.lo <mlo20@stuy.edu>
2021-10-13 04:03:56 -0400

    added working code from triogit status

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -1,15 +1,13 @@
 # Squidcatman - Michelle Lo & Hellokitty, Angela Zhang & Inky, Deven Maheshwari & Batman
 # SoftDev
-# K13 - //
-# October 8 2021
+# K13 - Template for Success/Using Flask/Creating flask app using the occupation python data 
+# October 13 2021
 
 from flask import Flask, render_template 
-import occupation
 import csv
 import random
 
 app = Flask(__name__)
-
 
 
 @app.route("/")
@@ -18,20 +16,37 @@ def hello_world():
 
 @app.route("/occupyflaskst") 
 def test_tmplt():
+    """
+    Creates occupyflaskst route on flask app. Uses a dictonary to take in occupation jobs and percentages and sends them to tablified.html. 
+    Parameters:
+        None
+    
+    Returns:
+        None
+    """
+
     jobp = {}
     with open('data/occupations.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
             job = row["Job Class"]
             percentage = float(row["Percentage"])
-            jobp[job] = str(percentage)
+            link = row["Link"]
+            jobp[job] = [str(percentage), link]
         del jobp['Total']
     return render_template( 'tablified.html', foo = "K13", random = chooseRandom(jobp), response = jobp)
 
-def chooseRandom(jobdict):     #
+def chooseRandom(jobdict):  
+    """
+    Chooses random occupation based on weighted averages (from occupation.py)
+    Parameters:
+        None
+    Returns:
+        k - job name
+    """
     randVal = random.uniform(0, 100) # randomly chooses a number from 0 to total percentage
     for k, v in jobdict.items():
-        randVal -= float(v)
+        randVal -= float(v[0])
         if randVal <= 0:    # first cumulative percentage >= random value is the job chosen
             return k
 
