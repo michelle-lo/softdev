3abdefacb110ed6e427e0474135f0e9bfb06a8b9
michelle.lo <mlo20@stuy.edu>
2021-10-13 04:28:05 -0400

    added some comments

--- 13_tempwork/app.py
+++ 13_tempwork/app.py
@@ -1,9 +1,9 @@
 # Squidcatman - Michelle Lo & Hellokitty, Angela Zhang & Inky, Deven Maheshwari & Batman
 # SoftDev
-# K13 - Template for Success/Using Flask/Creating flask app using the occupation python data 
+# K13 - Template for Success/Using Flask/Creating flask app using the occupation python data
 # October 13 2021
 
-from flask import Flask, render_template 
+from flask import Flask, render_template
 import csv
 import random
 
@@ -14,35 +14,35 @@ app = Flask(__name__)
 def hello_world():
     return "No hablo queso!"
 
-@app.route("/occupyflaskst") 
+@app.route("/occupyflaskst")
 def test_tmplt():
     """
-    Creates occupyflaskst route on flask app. Uses a dictonary to take in occupation jobs and percentages and sends them to tablified.html. 
+    Creates occupyflaskst route on flask app. Uses a dictonary to take in occupation jobs, percentages, and associated links and sends them to tablified.html.
     Parameters:
         None
-    
+
     Returns:
         None
     """
 
-    jobp = {}
-    with open('data/occupations.csv') as csvfile:
+    jobp = {} #creates dictionary
+    with open('data/occupations.csv') as csvfile: #reads csv file
         reader = csv.DictReader(csvfile)
         for row in reader:
-            job = row["Job Class"]
-            percentage = float(row["Percentage"])
-            link = row["Link"]
-            jobp[job] = [str(percentage), link]
-        del jobp['Total']
-    return render_template( 'tablified.html', foo = "K13", random = chooseRandom(jobp), response = jobp)
+            job = row["Job Class"] #name of job
+            percentage = float(row["Percentage"]) #percentage for job
+            link = row["Link"] #link for job
+            jobp[job] = [str(percentage), link] #the percentages and link are now values of a key
+        del jobp['Total'] #gets rid of "total" from the list of occupations
+    return render_template( 'tablified.html', foo = "K13", random = chooseRandom(jobp), response = jobp) #corresponds to the vars in tablified (foo is assignment #)
 
-def chooseRandom(jobdict):  
+def chooseRandom(jobdict):
     """
-    Chooses random occupation based on weighted averages (from occupation.py)
+    Chooses random occupation based on weighted averages.
     Parameters:
-        None
+        jobdict - dictionary created of csv file
     Returns:
-        k - job name
+        k - random job name
     """
     randVal = random.uniform(0, 100) # randomly chooses a number from 0 to total percentage
     for k, v in jobdict.items():
