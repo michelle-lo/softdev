1d367d69f76f3c20c33906f060d6459bd3f38cdc
michelle.lo <mlo20@stuy.edu>
2021-11-24 07:54:26 -0500

    updated with randomApod which loads random image and associated explanation

--- 19_api/app.py
+++ 19_api/app.py
@@ -23,6 +23,26 @@ def NASA_key():
         image = img,
         explanation = expl
     )
+    
+@app.route("/random")
+def randomApod():
+    #retrieval and loading of JSON information from apod request to NASA
+    page = urllib.request.urlopen(
+        "https://api.nasa.gov/planetary/apod?api_key=w3dlvp3pigesf9dslufCffVjJUJAoglsX64cF5lK&count=1")  # opens URL
+    # reads and loads JSON into a python dictionary
+    dict = json.loads(page.read())[0] #the returned object from the API is a list -> see count query for apod
+    img = dict.get('url')  # obtain picture URL through dictionary
+    expl = dict.get('explanation')  # obtain explanation of image
+
+    # print(dict)
+    # print(dict.get('explanation'))
+    # print(dict.get('img'))
+
+    return render_template(
+        'main.html',
+        image=img,
+        explanation=expl
+    )
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
