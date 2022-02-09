3731cef367205b15f5146a62f1ba5bd3f129a261
michelle.lo <mlo20@stuy.edu>
2022-02-08 22:53:12 -0500

    comments for k28

---
+++ k28_jsdom/dommy_consolery.js
@@ -0,0 +1,93 @@
+// Team HAM :: Hebe Huang, Andrew Juang, Michelle Lo
+// SoftDev pd2
+// K28 -- Getting more comfortable with the dev console and the DOM
+// 2022-02-08t
+// --------------------------------------------------
+
+
+//send diagnostic output to console
+//(Ctrl-Shift-K in Firefox to reveal console)
+console.log("AYO"); //ayo
+
+var i = "hello";
+var j = 20;
+console.log(i) //sends "hello" to console
+console.log(j) //sends 20 to console
+
+//assign an anonymous fxn to a var
+var f = function(x) {
+  var j=30; //j is 30 only in the scope of this function (otherwise, it would be 20).
+  return j+x;
+};
+console.log(f(i)) //30hello
+console.log(f(j)) //50
+
+
+//instantiate an object
+//similar to a dictionary!
+var o = { 'name' : 'Thluffy',
+          age : 15,
+          items : [10, 20, 30, 40],
+          morestuff : {a : 1, b : 'ayo'},
+          func : function(x) { //alternative way of defining a function
+            return x+30;
+          }
+        };
+//console.log(o) //will print the object in the console
+
+//can print specific aspects of an object (instance variables in java terms)
+//using the syntax used to get the value of a key in a dictionary.
+console.log(o.age) //15
+console.log(o.items[1]) //20
+console.log(o.morestuff.b) //ayo
+console.log(o.func(j)) //50
+console.log(o.func(i)) //hello30
+
+
+
+var addItem = function(text) {
+  var list = document.getElementById("thelist"); //corresponds to an element on the html file called "thelist"
+  var newitem = document.createElement("li");
+  newitem.innerHTML = text;
+  list.appendChild(newitem);
+};
+addItem("NEW ITEM") //adds a new item called "new item"
+addItem(3) //also allows numbers
+
+
+var removeItem = function(n) {
+  var listitems = document.getElementsByTagName('li');
+  listitems[n].remove();
+};
+removeItem(9) //removes previously added 3
+//removeItem(10) //produces error
+
+
+var red = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    items[i].classList.add('red');
+  }
+};
+red() //will turn the first and last elements red
+//li elements whose class lists are either empty or have red at the end will be red.
+//there seems to be an order of importance for colors as defined in the style sheet: red < green < blue.
+//the color that appears later in the style sheet overrides the previous color and comes up on top as the alpha color!!
+
+
+var stripe = function() {
+  var items = document.getElementsByTagName("li");
+  for(var i = 0; i < items.length; i++) {
+    if (i%2==0){
+      items[i].classList.add('red');
+    } else {
+      items[i].classList.add('blue');
+    }
+  }
+};
+stripe()
+
+//insert your implementations here for...
+// FIB
+// FAC
+// GCD
